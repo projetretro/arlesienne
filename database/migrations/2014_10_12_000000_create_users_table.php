66a3a51736aa5d3030e0645b9359f3319ff671f0
Ophélien <ophelien.duparc@projetretro.io>
2021-12-21 18:16:05 +0100

    Added migration for users, posts, categories, tags, contact requests and forbidden words

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name', 40)->nullable();
+            $table->string('last_name', 40)->nullable();
+            $table->string('pronouns')->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
---
+++ database/migrations/2021_12_21_170010_alter_post_tag_table_add_foreign_keys.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterPostTagTableAddForeignKeys extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('post_tag', function (Blueprint $table) {
+            //
+            $table->foreignId('post_id')->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+
+            $table->foreignId('tag_id')->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('post_tag', function (Blueprint $table) {
+            //
+            $table->dropConstrainedForeignId('post_id');
+            $table->dropConstrainedForeignId('tag_id');
+        });
+    }
+}
